90f73e198cb1afc57318ad84dd7d88c24f4e86db
webik123 <vadim.sw@hotmail.com>
2015-03-05 17:09:57 +0200

    getting started

---
+++ index.php
@@ -0,0 +1,39 @@
+﻿
+<html>
+	<head>
+		<title>Мій сайт</title>
+		<link rel="stylesheet" href="style.css" type="text/css"/>
+	</head>
+	<body>
+
+		<div class="header" align="center">
+			<!-- Верхняя часть страницы -->
+			
+			
+			<!-- Верхняя часть страницы -->
+		</div>
+
+		
+		<div class="light">
+			<!-- Навигация -->
+			
+			<ul>
+				<li><a href='index.php'>Головна</a></li>
+				<li><a href='index.php?id=contact'>Контакти</a></li>
+				<li><a href='index.php?id=about'>Новини</a></li>
+			</ul>
+			<!-- Навигация -->
+		</div>
+		<div class="center" align="center">
+			<h3>Новини</h3>
+		</div>
+		<div class="right">
+			
+		</div>
+		<div class="footer" align="center">
+			<!-- Нижняя часть страницы -->
+			&copy; Супер-мега сайт, 2015 
+			<!-- Нижняя часть страницы -->
+		</div>
+	</body>
+</html>
