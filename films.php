e62871a8cb099bcc0692edbab51d15fede5bf2c4
webik123 <vadim.sw@hotmail.com>
2015-03-26 19:20:15 +0200

    the output data

---
+++ films.php
@@ -0,0 +1,6 @@
+<?
+$db = mysqli_connect('localhost', 'root', '', 'mysite');
+$result = mysqli_query($db, "SELECT id, time_created, title, discription FROM film");
+while($row = mysqli_fetch_array($result))
+print_r('<b>' . $row['title'] . '</b>' . '<br>' . $row['discription'] . '<br>'. $row['time_created'] . '<br>');
+?>
