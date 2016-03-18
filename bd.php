a5f53abf226aa4667ecd45f38f5be513833ffc4c
webik123 <webik123@mail.ru>
2016-03-18 20:39:34 +0200

    new work

--- bd.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: дмитро
- * Date: 17.03.2016
- * Time: 20:14
- */
--- films.php
+++ films.php
@@ -1,7 +1,7 @@
 ﻿<?
 $db = mysqli_connect('localhost', 'root', '', 'mysite');
-$result = mysqli_query($db, "SELECT id, time_created, title, discription FROM film ORDER BY `id` DESC LIMIT 5");
+$result = mysqli_query($db, "SELECT * FROM film ORDER BY `id` DESC LIMIT 5");
 while($row = mysqli_fetch_array($result))
-print_r('<b>' . $row['title'] . '</b>' . '<br>' . $row['discription'] . '<br>'. $row['time_created'] . '<br>' . '<a href="addit.php">Редактировать новость</a>' . '<br>');
+print_r('<b>' . $row['text_1'] . '</b>' . '<br>' . $row['text_2'] . '<br>'. $row['time_created'] . '<br>' . '<a href="addit.php">Редактировать новость</a>' . '<br>');
 
 ?>
